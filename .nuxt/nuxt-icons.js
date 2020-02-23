46e483b0f6e48240e3d233e4ba8226c448156be9
allania7med11 <allania7med11@gmail.com>
2020-02-23 19:58:00 +0100

    .nuxt delete

--- .nuxt/nuxt-icons.js
+++
@@ -1,5 +0,0 @@
-export default async function (ctx, inject) {
-  const icons = {"64x64":"/_nuxt/icons/icon_64.e75293.png","120x120":"/_nuxt/icons/icon_120.e75293.png","144x144":"/_nuxt/icons/icon_144.e75293.png","152x152":"/_nuxt/icons/icon_152.e75293.png","192x192":"/_nuxt/icons/icon_192.e75293.png","384x384":"/_nuxt/icons/icon_384.e75293.png","512x512":"/_nuxt/icons/icon_512.e75293.png"}
-  const getIcon = size => icons[size + 'x' + size] || ''
-  inject('icon', getIcon)
-}
--- constants/env.js
+++ constants/env.js
@@ -1,14 +1,14 @@
-const Production = false
+const Production = true
 const host = Production ? "0.0.0.0" : "localhost"
 const urls = Production
   ? {
-      baseURL: "http://unix:/run/gunicornfull.sock",
-      browserBaseURL: "https://legacy.effectivewebapp.com/"
+      baseURL: "https://effectivewebapp.com/",
+      browserBaseURL: "https://effectivewebapp.com/"
     }
   : {
       baseURL: "http://localhost",
       browserBaseURL: "http://localhost"
     }
-const Test = true
+const Test = false
 const TestInfo = { user: "user1" }
 export { host, urls, Test, TestInfo }
