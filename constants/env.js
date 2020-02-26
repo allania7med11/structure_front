0f00f45f062490895f6afd3b75eab15ab624e4df
allania7med11 <allania7med11@gmail.com>
2020-02-26 16:41:49 +0100

    effective

--- constants/env.js
+++ constants/env.js
@@ -2,8 +2,8 @@ const Production = true
 const host = Production ? "0.0.0.0" : "localhost"
 const urls = Production
   ? {
-      baseURL: "https://test.effectivewebapp.com/",
-      browserBaseURL: "https://test.effectivewebapp.com/"
+      baseURL: "https://effectivewebapp.com/",
+      browserBaseURL: "https://effectivewebapp.com/"
     }
   : {
       baseURL: "http://localhost",
